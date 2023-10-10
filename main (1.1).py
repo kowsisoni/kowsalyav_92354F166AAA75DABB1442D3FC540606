75dda4ea8f9137cbcd6df86b9186dc16d248564f
kowsisoni <144417479+kowsisoni@users.noreply.github.com>
2023-10-10 19:41:50 +0530

    Add files via upload

---
+++ main (1.1).py
@@ -0,0 +1,12 @@
+def factorial(n):
+    # Base case: factorial of 0 is 1
+    if n == 0:
+        return 1
+    # Recursive case: factorial of n is n multiplied by factorial of (n-1)
+    else:
+        return n * factorial(n - 1)
+
+# Test the function
+number = int(input("Enter a number: "))
+result = factorial(number)
+print(f"Factorial of {number} is {result}")
